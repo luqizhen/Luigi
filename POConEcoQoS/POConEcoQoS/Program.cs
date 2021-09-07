b975e8e92327796b94dd3d689094123fe0ba7739
Luigi Lu <qizhen_lu@dell.com>
2021-09-07 12:15:33 +0800

    Revert "update"
    This reverts commit 8ccbd09cbb28b80d64d75b4073526f904c70b649.

--- POConEcoQoS/POConEcoQoS/Program.cs
+++ POConEcoQoS/POConEcoQoS/Program.cs
@@ -10,18 +10,17 @@ namespace POConEcoQoS
     {
         static void Main(string[] args)
         {
-            var process = Process.GetCurrentProcess();
+            var process = Process.GetProcessById(29260);
             Console.WriteLine(
                 $"Handle: {process.Handle}\n" +
                 $"Name: {process.ProcessName}\n" +
                 $"Priority: {process.PriorityClass}\n"
                 );
 
-            object processInfo;
+            object processInfo = null;
 
-            var rs = ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, out processInfo);
-            Console.WriteLine("GetProcessInfo(ProcessPowerThrottling): " + rs);
-            Console.WriteLine(processInfo);
+            //ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, out processInfo);
+            //Console.WriteLine(processInfo);
 
             WinAPI.PROCESS_POWER_THROTTLING_STATE pi = new WinAPI.PROCESS_POWER_THROTTLING_STATE { ControlMask = 1, StateMask = 1 };
             rs = ProcessInformationWrapper.SetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, pi);
