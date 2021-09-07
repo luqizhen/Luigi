7b9399ea9dfc73e8c27fc31a924eaf8929b047fe
Luigi Lu <qizhen_lu@dell.com>
2021-09-07 12:01:07 +0800

    update

--- POConEcoQoS/POConEcoQoS/Program.cs
+++ POConEcoQoS/POConEcoQoS/Program.cs
@@ -19,15 +19,20 @@ namespace POConEcoQoS
 
             object processInfo;
 
-            var rs = ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, out processInfo);
-            Console.WriteLine("GetProcessInfo(ProcessPowerThrottling): " + rs);
+            var rs_get = ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, out processInfo);
+            Console.WriteLine("GetProcessInfo(ProcessPowerThrottling): " + rs_get);
             Console.WriteLine(processInfo);
 
-            //ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessMemoryPriority, out processInfo);
-            //Console.WriteLine(processInfo);
+            WinAPI.PROCESS_POWER_THROTTLING_STATE pi = new WinAPI.PROCESS_POWER_THROTTLING_STATE
+            {
+                Version = WinAPI.PROCESS_POWER_THROTTLING_CURRENT_VERSION,
+                ControlMask = WinAPI.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+                StateMask = WinAPI.PROCESS_POWER_THROTTLING_EXECUTION_SPEED
+            };
 
-            //WinAPI.MEMORY_PRIORITY_INFORMATION pi = new WinAPI.MEMORY_PRIORITY_INFORMATION { MemoryPriority = 5 };
-            //ProcessInformationWrapper.SetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessMemoryPriority, pi);
+            var rs_set = ProcessInformationWrapper.SetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, pi);
+            Console.WriteLine("SetProcessInfo(ProcessPowerThrottling): " + rs_set);
+            Console.WriteLine(pi);
 
             //ProcessInformationWrapper.GetProcessInfo(process.Handle, WinAPI.PROCESS_INFORMATION_CLASS.ProcessMemoryPriority, out processInfo);
             //Console.WriteLine(processInfo);
